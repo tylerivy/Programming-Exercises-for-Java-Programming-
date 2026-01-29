dddf61ee2ea86b85b33baaf58d9d504885d1f11a
tylerivy <tylerivy03@gmail.com>
2026-01-29 15:04:22 -0500

    The first few programs at the end of Chapter7

---
+++ Chapter7SingleDimensionalArray/src/AnalyzeNumbers.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class AnalyzeNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many numbers would you like to enter? ");
+        int userInput = sc.nextInt();
+
+        double [] numbers = new double[userInput];
+
+        for(int i = 0; i < userInput; i++){
+            System.out.print("Enter a number: ");
+            numbers[i] = sc.nextDouble();
+        }
+
+        double sum = 0.0;
+        for(int i = 0; i < numbers.length; i++){
+            sum += numbers[i];
+        }
+
+        double average = sum / numbers.length;
+        int count = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > average){
+                count++;
+            }
+        }
+        System.out.printf("\n The number average of the numbers is %.2f. Numbers above the average: %d", average, count );
+    }
+}
---
+++ Chapter7SingleDimensionalArray/src/Calculator.java
@@ -0,0 +1,27 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        if(args.length != 3){
+            System.out.println("You must input operator1 operand operator2");
+            System.exit(1);
+        }
+
+        int result = 0;
+
+        switch(args[1]){
+            case "+":
+                result = Integer.parseInt(args[0]) + Integer.parseInt(args[2]);
+                break;
+            case "-":
+                result = Integer.parseInt(args[0]) - Integer.parseInt(args[2]);
+                break;
+            case "*":
+                result = Integer.parseInt(args[0]) * Integer.parseInt(args[2]);
+                break;
+            case "/":
+                result = Integer.parseInt(args[0]) / Integer.parseInt(args[2]);
+                break;
+        }
+        System.out.println(args[0] + " " + args[1] + " " + args[2] + " " + " = " + result);
+    }
+}
